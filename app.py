e55ee778c916b43560995576a207cc17d5fbd0ec
1 <a1@1s-MacBook-Pro.local>
2026-04-05 15:01:31 +0800

    modify app.py, add host and port

--- app.py
+++ app.py
@@ -34,4 +34,5 @@ def get_userinfo():
         return {'status':401, 'message': 'request method not allowed'}
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+    # app.run(debug=True)
